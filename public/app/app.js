db1a69c43dd572c3b63c235a823564dac024ae17
Eric Davis <davis.25811@gmail.com>
2018-01-16 12:56:47 -0500

    Added Angular service to handle websocket.

--- public/app/app.js
+++ public/app/app.js
@@ -21,3 +21,19 @@ app.factory('userProfile', function($resource) {
 app.factory('marketTickers', function($resource) {
     return $resource('http://localhost:3000/tickers/');
 });
+
+// This factory's code written by Brian Ford, many thanks!
+// https://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/#disqus_thread
+app.factory('socket', function($rootScope) {
+    var socket = io();
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    callback.apply(socket, args);
+                });
+            });
+        }
+    }
+});
--- public/app/controllers/TickerController.js
+++ public/app/controllers/TickerController.js
@@ -1,11 +1,17 @@
 angular.module('crypto')
-    .controller('TickerController', ['$scope', '$interval', 'marketTickers', function($scope, $interval, marketTickers) {
+    .controller('TickerController', ['$scope', '$interval', 'marketTickers', 'socket', function($scope, $interval, marketTickers, socket) {
         // Get tickers
         $scope.tickers = marketTickers.query();
         $interval(function() {
             $scope.tickers = marketTickers.query();
         }, 60000);
 
+
+        socket.on('refresh_tickers', function(data) {
+            var parsed = JSON.parse(data);
+            console.log(parsed.RAW.ETH.USD.PRICE);
+            $scope.sock = parsed;
+        });
         // Function for applying classes for positive/negative percent changes
         $scope.getPercentChangeClass = function(percentChange) {
             if (percentChange > 0) {
